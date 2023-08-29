481e5788298d8febf49400198425dfec3d7ebe50
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-29 19:55:33 -0300

    exer 4

---
+++ src/exercicio4.ts
@@ -0,0 +1,30 @@
+class Carro {
+  marca?: string;
+  modelo?: string;
+  ano?: number;
+
+
+  constructor(marca?:string,modelo?:string,ano?:number);
+  constructor(marca:string,modelo:string,ano?:number);
+  constructor(marca:string,modelo:string,ano:number){
+    if( marca !== undefined){
+        this.marca = marca;
+    }
+    if( modelo !== undefined){
+        this.modelo = modelo;
+    }
+    if( ano !== undefined){
+        this.ano = ano;
+    }
+  }
+  print(): void {
+    console.log(this);
+  }
+}
+
+const g = new Carro("VW", "Gol", 2010);
+g.print();
+const f = new Carro("Fiat", "Uno");
+f.print();
+const v = new Carro();
+v.print();
